e088d05f3df2e5000cc9d698cc953612456ae54a
ashlae blum'e <laelume@gmail.com>
2025-09-22 12:18:54 +1200

    Create __init__.py

---
+++ utils/scraper/__init__.py
@@ -0,0 +1,6 @@
+"""ANVO SCRAP - Animal Vocalization Web Scraper - SCRAP.PY/SCRAP.R"""
+
+from .scrap import scrape, list_sources, get_source_info
+
+__version__ = "1.0.0"
+__all__ = ["scrape", "list_sources", "get_source_info"]
